04796dfd939fb191dd3c848477b305c51ccdd38d
toomassoots <toomas.soots@mail.ee>
2020-10-16 17:16:48 +0300

    Node

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.get('/api/users/:id', (req,res)=>{
 
         };
         users.push(newUser)
-        delete newUser.password;
+    //    delete newUser.password;
         res.status(201).json({
          
             success: true,
@@ -78,6 +78,62 @@ app.get('/api/users/:id', (req,res)=>{
 
  })
 
+//PUT
+//required: id
+//Optional: firstname, lastname password, email
+app.put('/api/users/', (req, res)=> {
+
+    const id = typeof(req.body.id)==='number'? req.body.id : false;
+    const firstName = typeof(req.body.firstName)==='string' && req.body.firstName.trim().length>0?req.body.firstName : false;
+    const lastName = typeof(req.body.lastName)==='string' && req.body.lastName.trim().length>0?req.body.lastName : false;
+    const email = typeof(req.body.email)==='string' && req.body.email.trim().length>0?req.body.email : false;
+    const password = typeof(req.body.password)==='string' && req.body.password.trim().length>3?req.body.password : false;
+    if(id){
+        if(firstName){
+            users[id].firstName = firstName
+        }
+        if(lastName){
+            users[id].lastName = lastName
+        }
+        if(email){
+            users[id].email = email
+        }
+        if(password){
+            users[id].password = password
+        }
+        res.status(201).json({
+         
+            success: true,
+            user:users[id]
+           
+        });
+    }else{
+        res.status(400).json({
+         
+            success: false,
+            message: 'Required field(s) missing'
+        })
+    }
+})
+app.delete('/api/users/', (req, res)=>{
+    const id = typeof(req.body.id)==='number'? req.body.id : false;
+    if(id){
+        users.splice(id, 1)
+        res.status(200).json({
+         
+            success: true
+            
+           
+        });
+    }else{
+        res.status(400).json({
+         
+            success: false,
+            message: 'Required field(s) missing'
+        })
+    }
+
+})
 app.listen(3000, ()=>{
     console.log('Server running')
 })
